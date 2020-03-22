9560e6f6a31e7352f54734e9001cbc3f0ee6f76a
lucievr <lucy.vrsovska@gmail.com>
2020-03-22 21:14:57 +0100

    Create screens, add ProductNavigator and ProductItem component with styling

---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+// import { createBottomTabNavigator } from 'react-navigation-tabs';
+// import { createDrawerNavigator } from 'react-navigation-drawer';
+
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import Colors from '../constants/Colors';
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+    }
+  }
+);
+
+export default createAppContainer(ProductsNavigator);
